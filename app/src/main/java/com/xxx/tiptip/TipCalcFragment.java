90fc45940b0e2f2df97ea6e48a7be49e98c09065
Exin Coda <ggaao1994@gmail.com>
2016-04-05 13:51:30 +0800

    TipCalcFragment 结构优化

--- app/src/main/java/com/xxx/tiptip/TipCalcFragment.java
+++ app/src/main/java/com/xxx/tiptip/TipCalcFragment.java
@@ -17,14 +17,13 @@ import java.text.DecimalFormat;
 
 public class TipCalcFragment extends Fragment {
 
-    static ViewHolder viewHolder;
-
-    public TipCalcFragment() {}
+    public static ViewHolder viewHolder;
 
     public static void updateView() {
-        viewHolder.tv_TipAmount.setText(format2digit(CalcCore.tipAmount));
-        viewHolder.tv_TotalAmount.setText(format2digit(CalcCore.totalAmount));
-        viewHolder.tv_AmountPerPerson.setText(format2digit(CalcCore.amountPerPerson));
+        viewHolder.updateView();
+    }
+
+    public TipCalcFragment() {
     }
 
     @Override
@@ -39,164 +38,17 @@ public class TipCalcFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_tip_calc, container, false);
-
         viewHolder = new ViewHolder(view);
-
-        viewHolder.et_bill.setText(format2digit(CalcCore.bill));
-        viewHolder.et_percentage.setText(formatPercentage(CalcCore.percentageOfTip));
-        viewHolder.et_number_of_people.setText(String.format("%d", CalcCore.numberOfPeople));
-
-        viewHolder.et_bill.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
-            }
-
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-
-            }
-
-            @Override
-            public void afterTextChanged(Editable s) {
-                if (s.length() > 0) {
-                    CalcCore.bill = Double.valueOf(s.toString());
-                    calculate();
-                }
-            }
-        });
-        viewHolder.et_bill.setOnFocusChangeListener(new View.OnFocusChangeListener() {
-            @Override
-            public void onFocusChange(View v, boolean hasFocus) {
-                if (hasFocus) {
-                    viewHolder.et_bill.setText("");
-                } else {
-                    viewHolder.et_bill.setText(format2digit(CalcCore.bill));
-                }
-            }
-        });
-
-        viewHolder.et_percentage.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
-            }
-
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-
-            }
-
-            @Override
-            public void afterTextChanged(Editable s) {
-                if (s.length() > 0) {
-                    CalcCore.percentageOfTip = Double.valueOf(s.toString()) / 100;
-                    calculate();
-                }
-            }
-        });
-        viewHolder.et_percentage.setOnFocusChangeListener(new View.OnFocusChangeListener() {
-            @Override
-            public void onFocusChange(View v, boolean hasFocus) {
-                if (hasFocus) {
-                    viewHolder.et_percentage.setText("");
-                } else {
-                    viewHolder.et_percentage.setText(formatPercentage(CalcCore.percentageOfTip));
-                }
-            }
-        });
-
-        viewHolder.et_number_of_people.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
-            }
-
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-
-            }
-
-            @Override
-            public void afterTextChanged(Editable s) {
-                if (s.length() > 0) {
-                    CalcCore.numberOfPeople = Integer.valueOf(s.toString());
-                    calculate();
-                }
-            }
-        });
-        viewHolder.et_number_of_people.setOnFocusChangeListener(new View.OnFocusChangeListener() {
-            @Override
-            public void onFocusChange(View v, boolean hasFocus) {
-                if (hasFocus) {
-                    viewHolder.et_number_of_people.setText("");
-                } else {
-                    viewHolder.et_number_of_people.setText(String.format("%d", CalcCore.numberOfPeople));
-                }
-            }
-        });
-
-        viewHolder.btnTipPlus.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                CalcCore.percentageOfTip += 0.005;
-                if (viewHolder.et_percentage.hasFocus())
-                    viewHolder.et_percentage.setText(String.format("%.2f", CalcCore.percentageOfTip * 100));
-                else
-                    viewHolder.et_percentage.setText(formatPercentage(CalcCore.percentageOfTip));
-                calculate();
-            }
-        });
-
-        viewHolder.btnTipMinus.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                CalcCore.percentageOfTip =Math.max(CalcCore.percentageOfTip - 0.005, 0);
-                if (viewHolder.et_percentage.hasFocus())
-                    viewHolder.et_percentage.setText(String.format("%.2f", CalcCore.percentageOfTip * 100));
-                else
-                    viewHolder.et_percentage.setText(formatPercentage(CalcCore.percentageOfTip));
-                calculate();
-            }
-        });
-
-        viewHolder.btnPeoplePlus.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                CalcCore.numberOfPeople += 1;
-                viewHolder.et_number_of_people.setText(String.format("%d", CalcCore.numberOfPeople));
-                calculate();
-            }
-        });
-
-        viewHolder.btnPeopleMinus.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                CalcCore.numberOfPeople = Math.max(CalcCore.numberOfPeople - 1, 1);
-                viewHolder.et_number_of_people.setText(String.format("%d", CalcCore.numberOfPeople));
-                calculate();
-            }
-        });
-
         calculate();
-
         return view;
     }
 
-    static String format2digit(double price) {
-        return new DecimalFormat("#.00").format(price);
-    }
-
-    static String formatPercentage(double percentage) {
-        return String.format("%.2f", percentage * 100);
-    }
-
-    static void calculate() {
+    void calculate() {
         CalcCore.Calc calc = new CalcCore.Calc();
         calc.execute();
     }
 
-    private static class ViewHolder {
+    private class ViewHolder {
         public TextView tv_TipAmount, tv_TotalAmount, tv_AmountPerPerson;
         public EditText et_bill, et_percentage, et_number_of_people;
         public Button btnTipPlus, btnTipMinus, btnPeoplePlus, btnPeopleMinus;
@@ -212,6 +64,149 @@ public class TipCalcFragment extends Fragment {
             btnTipMinus = (Button) view.findViewById(R.id.btn_tip_minus);
             btnPeoplePlus = (Button) view.findViewById(R.id.btn_people_plus);
             btnPeopleMinus = (Button) view.findViewById(R.id.btn_people_minus);
+
+            //init EditTexts with default values
+            et_bill.setText(format2digit(CalcCore.bill));
+            et_percentage.setText(formatPercentage(CalcCore.percentageOfTip));
+            et_number_of_people.setText(formatInteger(CalcCore.numberOfPeople));
+
+            et_bill.addTextChangedListener(new TextWatcher() {
+                @Override
+                public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                }
+
+                @Override
+                public void onTextChanged(CharSequence s, int start, int before, int count) {
+                }
+
+                @Override
+                public void afterTextChanged(Editable s) {
+                    if (s.length() > 0) {
+                        CalcCore.bill = Double.valueOf(s.toString());
+                        calculate();
+                    }
+                }
+            });
+            et_bill.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+                @Override
+                public void onFocusChange(View v, boolean hasFocus) {
+                    if (hasFocus) {
+                        et_bill.setText("");
+                    } else {
+                        et_bill.setText(format2digit(CalcCore.bill));
+                    }
+                }
+            });
+
+            et_percentage.addTextChangedListener(new TextWatcher() {
+                @Override
+                public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                }
+
+                @Override
+                public void onTextChanged(CharSequence s, int start, int before, int count) {
+                }
+
+                @Override
+                public void afterTextChanged(Editable s) {
+                    if (s.length() > 0) {
+                        CalcCore.percentageOfTip = Double.valueOf(s.toString()) / 100;
+                        calculate();
+                    }
+                }
+            });
+            et_percentage.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+                @Override
+                public void onFocusChange(View v, boolean hasFocus) {
+                    if (hasFocus) {
+                        et_percentage.setText("");
+                    } else {
+                        et_percentage.setText(formatPercentage(CalcCore.percentageOfTip));
+                    }
+                }
+            });
+
+            et_number_of_people.addTextChangedListener(new TextWatcher() {
+                @Override
+                public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                }
+
+                @Override
+                public void onTextChanged(CharSequence s, int start, int before, int count) {
+                }
+
+                @Override
+                public void afterTextChanged(Editable s) {
+                    if (s.length() > 0) {
+                        CalcCore.numberOfPeople = Integer.valueOf(s.toString());
+                        calculate();
+                    }
+                }
+            });
+            et_number_of_people.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+                @Override
+                public void onFocusChange(View v, boolean hasFocus) {
+                    if (hasFocus) {
+                        et_number_of_people.setText("");
+                    } else {
+                        et_number_of_people.setText(formatInteger(CalcCore.numberOfPeople));
+                    }
+                }
+            });
+
+            btnTipPlus.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    CalcCore.percentageOfTip += 0.005;
+                    et_percentage.setText(formatPercentage(CalcCore.percentageOfTip));
+                    calculate();
+                }
+            });
+
+            btnTipMinus.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    CalcCore.percentageOfTip = Math.max(CalcCore.percentageOfTip - 0.005, 0);
+                    et_percentage.setText(formatPercentage(CalcCore.percentageOfTip));
+                    calculate();
+                }
+            });
+
+            btnPeoplePlus.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    CalcCore.numberOfPeople += 1;
+                    et_number_of_people.setText(formatInteger(CalcCore.numberOfPeople));
+                    calculate();
+                }
+            });
+
+            btnPeopleMinus.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    CalcCore.numberOfPeople = Math.max(CalcCore.numberOfPeople - 1, 1);
+                    et_number_of_people.setText(formatInteger(CalcCore.numberOfPeople));
+                    calculate();
+                }
+            });
+        }
+
+        String format2digit(double price) {
+            return String.format("%.2f", price);
+        }
+
+        String formatPercentage(double percentage) {
+            return String.format("%.1f", percentage * 100);
+        }
+
+        String formatInteger(int i) {
+            return String.format("%d", i);
+        }
+
+        public void updateView() {
+            tv_TipAmount.setText(format2digit(CalcCore.tipAmount));
+            tv_TotalAmount.setText(format2digit(CalcCore.totalAmount));
+            tv_AmountPerPerson.setText(format2digit(CalcCore.amountPerPerson));
         }
     }
 }
